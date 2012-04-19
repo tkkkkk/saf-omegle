ead03e7a81050a5add0c6f51b53bb6fd5bb8dece
omegleuser.22334@gmail.com <omegleuser.22334@gmail.com@fc821268-7c8e-f2b4-aa26-1cb86917af95>
2012-04-19 03:14:31 +0000

    Windows bugfix?

--- trunk/launcher.py
+++ trunk/launcher.py
@@ -3,7 +3,6 @@ import urllib2
 import win32api
 import win32con
 
-from win32com.shell import shell
 from win32com.shell import shellcon
 
 global chatboturl
@@ -37,6 +36,7 @@ def file_in_special_path(specialpath, fname):
     @param specialpath: The CSIDL path found in shellcon
     @param fname: the name of the file
     """
+    from win32com.shell import shell
     path = shell.SHGetFolderPath(0, specialpath, None, 0)
     path = os.path.join(path, fname)
     return path
